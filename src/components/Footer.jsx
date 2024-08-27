ba55839c7afc1f2b935733fb813b33ca92adab51
YanDSilva <yandsilva94@gmail.com>
2024-08-27 11:47:16 -0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+export default function Footer() {
+  return (
+    <div>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        <div>
+          <img src={assets.logo} className="mb-5 w-32" alt="" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+            explicabo et quod similique distinctio eaque, cum soluta, blanditiis
+            veniam natus id sed libero nesciunt error ipsa maiores molestiae,
+            quas cupiditate!
+          </p>
+        </div>
+
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+55 14 9999-9999</li>
+            <li>contact@foreveryou.com</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright 2024@ forever.com - All Right Reserved.
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/context/ShopContext.jsx
@@ -0,0 +1,26 @@
+import { createContext, useState } from "react";
+import { products } from "../assets/assets";
+
+export const ShopContext = createContext();
+
+const ShopContextProvider = (props) => {
+  const currency = "$";
+  const delivery_free = 10;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
+
+  const value = {
+    products,
+    currency,
+    delivery_free,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
+  };
+  return (
+    <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
+  );
+};
+
+export default ShopContextProvider;
